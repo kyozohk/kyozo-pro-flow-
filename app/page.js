a7dfe410b221d72bcf1670bd15eb413dbb01509b
kyozohk <ashok@kyozo.com>
2025-08-03 14:01:19 +0800

    * Keeping things simple

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 'use client'
 import { ConnectExplore, Hero, Footer, Toolkit, Bottom, Marquee, BubbleMarquee, Plans, RevealText, ParallaxCardsContainer } from "../components";
+import Exclusive from "../components/cards/Exclusive";
+import Engage from "../components/cards/Engage";
+import Grow from "../components/cards/Grow";
 
 export default function Home() {
   return (
     <div className="relative overflow-x-hidden">
       <Hero />
       <ConnectExplore />  
-      <RevealText text="Where creative minds converge" />
-      <ParallaxCardsContainer />
-      
+      <Exclusive />
+      <Engage />
+      <Grow />
+      {/* <RevealText text="Where creative minds converge" /> */}
+      {/* <ParallaxCardsContainer />       */}
       <Toolkit />      
       <Marquee />
       <Plans />
